2c4399d8f33dcb397fbee76da4de21bfbd7d8e21
Charbel Allawi <charbelallawiwork@gmail.com>
2023-08-07 01:08:51 +0300

    addorupdatecontact api

--- back-end/app/Http/Controllers/ContactController.php
+++ back-end/app/Http/Controllers/ContactController.php
@@ -16,7 +16,30 @@ class ContactController extends Controller
             $contacts = Contact::all();
             return response()->json(['contacts' => $contacts]);
         }
-    }  public function removecontact($id)
+    }
+    public function removecontact($id)
     {
         $contact = Contact::find($id)->delete();
     }
+
+
+    public function addOrUpdateContact(Request $request, $id = null)
+    {
+        if ($id === null || $id === "add") {
+            $contact = new Contact();
+        } else {
+            $contact = Contact::find($id);
+
+            if (!$contact) {
+                return response()->json(['error' => 'Contact not found'], 404);
+            }
+        }
+        $contact->username = $request->input('username', $contact->username);
+        $contact->phonenumber = $request->input('phonenumber', $contact->phonenumber);
+        $contact->address = $request->input('address', $contact->address);
+
+        $contact->save();
+
+        return response()->json(["contact" => $contact]);
+    }
+}
